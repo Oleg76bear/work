6938e44a018d017f06b190707066c781b8d3ad5b
Oleg76bear <122482236+Oleg76bear@users.noreply.github.com>
2023-01-23 18:46:31 +0300

    Проверка числа являющегося ли / на 7

---
+++ src/Task7.java
@@ -0,0 +1,10 @@
+public class Task7 {
+    public static void main(String[] args) {
+         int n = 8657;
+         if (n % 10 == 7) {
+             System.out.println(true);
+         } else
+             System.out.println(false);
+         }
+    }
+
