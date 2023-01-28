023eb1a788269a0c23d3fd055a76132f3db24ce0
Oleg76bear <122482236+Oleg76bear@users.noreply.github.com>
2023-01-28 23:40:01 +0300

    Проверка числа являющегося ли / на 7

---
+++ src/Task17.java
@@ -0,0 +1,14 @@
+public class Task17 {
+    public static void main(String[] args) {
+
+        for (int k = 1; ; k++) {
+            if (k % 7 == 0) {
+                System.out.println(k + " Hope!");
+            }
+                if (k == 100) {
+                    break;
+                }
+            }
+        }
+    }
+
