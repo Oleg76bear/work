d462d3391dd20df61781d8b92e0e9bc69a8639ba
Oleg76bear <122482236+Oleg76bear@users.noreply.github.com>
2023-02-11 20:54:04 +0300

    Класс "Car" представляет собой автомобиль

--- src/by/itacademy/classwork/figure/Figure.java
+++
@@ -1,5 +0,0 @@
-package by.itacademy.task.ClassWork.figure;
-
-public abstract class Figure {
-
-}
--- src/by/itacademy/classwork/figure/f2d/Square.java
+++
@@ -1,7 +0,0 @@
-package by.itacademy.door.ClassWork.figure.f2d;
-
-public class Square extends Box {
-   public Square (int a) {
-       super(a, a);
-   }
-}
